d9ec61e57661d5281cbd96fa4a3f0046a7a55aba
Cassia <ra-22265008-2@alunos.unicesumar.edu.br>
2023-11-10 16:51:43 -0300

    Curso finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
   title = 'app-lista-de-compras';
 
-  constructor() { }
+  listaDeCompra! : Array<Item>;
+  itemParaSerEditado! : Item;
+
+  constructor(
+    private listaService: ListaDeCompraService
+  ) { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra();
+    console.log(this.listaDeCompra)
+  }
+
+  editarItem(item: Item){
+    this.itemParaSerEditado = item;
+  }
+
+  deletarItem(itemId: number){
+    const index = this.listaDeCompra.findIndex((item) => item.id == itemId);
+    this.listaDeCompra.splice(index, 1);
+  }
+
+  limparLista(){
+    this.listaDeCompra = [];
+  }
+  ngDoCheck(): void {
+    console.log('DoCheck foi chamado');
+    this.listaService.atualizarLocalStorage();
+  }
 }
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, OnChanges, Output, OnDestroy } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
-
+import { Item } from 'src/app/interfaces/iItem';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoIdParaDeletar = new EventEmitter();
 
   faPen = faPen;
   faTrash = faTrash
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
 
+   }
+
+  ngOnChanges(){
+  }
+
+  editarItem(){
+    this.emitindoItemParaEditar.emit(this.item);
+  }
+
+deletarItem(){
+  this.emitindoIdParaDeletar.emit(this.item.id);
+}
+
+  checarItem(){
+    if(this.item.comprado == true){
+      this.item.comprado = false;
+    } this.item.comprado = true;
+  }
+
+  ngOnDestroy(){
+    console.log("Conseguiram me calar")
+  }
 }
